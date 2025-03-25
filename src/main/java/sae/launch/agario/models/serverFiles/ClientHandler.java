74229651e19fb33cd82f9467902fdb44a53fa5fc
rabute231 <antoinerabute@gmail.com>
2025-03-25 13:14:44 +0100

    Ajout des fichiers de gestion du serveur

---
+++ src/main/java/sae/launch/agario/models/serverFiles/ClientHandler.java
@@ -0,0 +1,54 @@
+package sae.launch.agario.models;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+public class ClientHandler extends Thread {
+	
+	private InputStream inputStream;
+	private OutputStream outputStream;
+	private PrintWriter printWriter;
+	private Server server;
+	
+	public ClientHandler(InputStream inputStream, OutputStream outputStream, Server serv) {
+		this.inputStream = inputStream;
+		this.outputStream = outputStream;
+		this.server = serv;
+	}
+
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+
+	public OutputStream getOutputStream() {
+		return outputStream;
+	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			byte[] buffer = new byte[1024]; 
+			int bytesRead;
+			
+			try {
+				bytesRead = this.getInputStream().read(buffer);
+				if (bytesRead != -1) {
+				    String messageRecu = new String(buffer, 0, bytesRead);
+				    System.out.println("Message du client : " + messageRecu);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				this.server.deleteClient(this, printWriter);
+				break;
+			}
+			
+		}
+	}
+
+	public void setPrintWriter(PrintWriter printWriter) {
+		this.printWriter = printWriter;
+	}
+	
+}
---
+++ src/main/java/sae/launch/agario/models/serverFiles/ServerSocketRunnerConnections.java
@@ -0,0 +1,28 @@
+package sae.launch.agario.models;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerSocketRunnerConnections implements Runnable{
+	
+	private Server server;
+	
+	public ServerSocketRunnerConnections(Server serv) {
+		this.server = serv;
+	}
+    @Override
+    public void run() {
+    	try {
+			ServerSocket serverSocket = new ServerSocket(8081);
+			while(true) {
+				Socket socket = serverSocket.accept();
+				server.addClient(socket);
+    		}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    }
+}
