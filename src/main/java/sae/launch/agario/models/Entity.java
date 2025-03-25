2891f81746ee16939299f05bec874846e7faa5e9
jean237 <jean237@unicaen.fr>
2025-03-25 12:49:33 +0100

    enity model

---
+++ src/main/java/sae/launch/agario/models/Entity.java
@@ -0,0 +1,41 @@
+public abstract class Entity {
+    private final int ID;
+    private double x;
+    private double y;
+    private double mass;
+
+    public Entity(int ID, double x, double y, double mass){
+        this.ID = ID;
+        this.x = x;
+        this.y = y;
+        this.mass = mass;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+}
