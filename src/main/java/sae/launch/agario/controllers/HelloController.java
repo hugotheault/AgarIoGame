a4f93d938e977eba5721d009c2999559bd826d4c
hugo theault <hugo.theault@etu.unicen.fr>
2025-03-25 10:39:37 +0100

    [FEAT] : adding playing view

--- src/main/java/sae/launch/agario/controllers/HelloController.java
+++ src/main/java/sae/launch/agario/controllers/HelloController.java
@@ -1,14 +1,34 @@
 package sae.launch.agario.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onLocalButtonClick(ActionEvent event) throws IOException {
+        welcomeText.setText("Lancement du jeu en Solo!");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/sae/launch/agario/InGameView.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    @FXML
+    protected void onMultiButtonClick(){
+        welcomeText.setText("Lancement du jeu en Multijoueur!");
     }
 }
