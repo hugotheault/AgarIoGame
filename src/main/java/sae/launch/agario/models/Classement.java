c0ef1fa6e41cb1472c1fa473942edb14e55579f9
hugo theault <hugo.theault@etu.unicen.fr>
2025-03-26 13:20:08 +0100

    [FIX] fix ranking

--- src/main/java/sae/launch/agario/models/Classement.java
+++ src/main/java/sae/launch/agario/models/Classement.java
@@ -13,20 +13,28 @@ public class Classement {
     }
 
     public void updateClassement() {
-        // Triez la liste des joueurs en fonction de leur masse (ordonnée de manière décroissante)
         Collections.sort(classement, new Comparator<Player>() {
             @Override
             public int compare(Player p1, Player p2) {
-                // Comparer les masses des joueurs (ordre décroissant)
                 return Double.compare(p2.getMass(), p1.getMass());
             }
         });
-
-        // Optionnel : Mettre à jour le classement si tu veux afficher les 3 premiers par exemple
-        // Mais il est déjà trié, donc tu peux juste afficher les 3 premiers joueurs :
         for (int i = 0; i < Math.min(3, classement.size()); i++) {
             Player p = classement.get(i);
             System.out.println("Classement " + (i + 1) + ": " + p.getID() + " - " + p.getMass());
         }
+    }
+
+    public void addPlayer(Player p) {
+        classement.add(p);
+    }
+
+    public void removePlayer(Player p) {
+        classement.remove(p);
+    }
+
+    public Player getPlayer(Integer indice) {
+        updateClassement();
+        return classement.get(indice);
     }
 }
