edf98f2e66fcc6a34514f1a3d36a0640606383cc
Nathan Chirac <chiracnathan59@gmail.com>
2025-03-26 12:44:09 +0100

    [FEAT] : Adding comment in Pellet

--- src/main/java/sae/launch/agario/models/Pellet.java
+++ src/main/java/sae/launch/agario/models/Pellet.java
@@ -4,14 +4,26 @@ import javafx.scene.paint.Color;
 import java.util.Random;
 
 public class Pellet extends Entity {
-    private final Color color;
+    private final Color color; // Color of the circle on the game display
 
-    public Pellet(int ID, double x, double y, double masse) {
-        super(ID, x, y, masse);
+    /**
+     *
+     * @param ID ID of the pellet randomly generated with IDGenerator
+     * @param x X-axis of the pellet
+     * @param y Y-axis of the pellet
+     * @param weight weight of the pellet, 10 by default
+     */
+    public Pellet(int ID, double x, double y, double weight) {
+        super(ID, x, y, weight
+        );
         Random random = new Random();
         this.color = Color.color(random.nextDouble(), random.nextDouble(), random.nextDouble());
     }
 
+    /**
+     *
+     * @return the color of the pellet
+     */
     public Color getColor() {
         return color;
     }
