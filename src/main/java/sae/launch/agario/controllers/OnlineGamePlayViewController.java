aa2a02a576df6bc79471927892153df3f22c5c73
rabute231 <antoinerabute@gmail.com>
2025-03-27 18:27:49 +0100

    Fixed

--- src/main/java/sae/launch/agario/controllers/OnlineGamePlayViewController.java
+++ src/main/java/sae/launch/agario/controllers/OnlineGamePlayViewController.java
@@ -17,6 +17,7 @@ import static sae.launch.agario.models.App.loadFXML;
 
 public class OnlineGamePlayViewController implements Initializable {
 
+    private FXMLLoader loader;
     private int ID;
     private String stringId;
     private static Scene scene;
@@ -39,14 +40,18 @@ public class OnlineGamePlayViewController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        loader = new FXMLLoader(getClass().getResource("/sae/launch/agario/InGameView.fxml"));
 
+        try {
+            loader.setController(new OnlineInGameController(inputIPCo, inputPortCo));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     public void onButtonClick(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/sae/launch/agario/InGameView.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        loader.setController(new OnlineInGameController(inputIPCo, inputPortCo));
         Parent root = loader.load();
         stage.setScene(new Scene(root));
         stage.show();
